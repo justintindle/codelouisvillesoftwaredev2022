b32f00aa85b0d113785c88dccc432d47698ed8c6
Justin Tindle <Justin.Tindle@eblusolutions.com>
2022-09-08 18:42:04 -0400

    Added a writeline

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("This is updating my code in my repo!");
         }
     }
 }
