af5ac50e1fb5e16edada9f8fd4c6dd18d214b1a3
Justin Tindle <Justin.Tindle@eblusolutions.com>
2022-09-08 18:57:13 -0400

    Added a folder and car class.

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("This is updating my code in my repo!");
+            Console.WriteLine("This is updating my in my repo!");
         }
     }
 }
---
+++ Test/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Test
+{
+    internal class Car
+    {
+        public string Model { get; set; }
+        public string Make { get; set; }
+        public DateTime Year { get; set; }
+        public int Count { get; set; }
+
+    }
+}
